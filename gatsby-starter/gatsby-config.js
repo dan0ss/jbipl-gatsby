25b3732476f5af56d34a50e0068ba172959c921c
Dan <d_emery@outlook.com.au>
2020-01-04 04:30:02 +0800

    Update gatsby-config.js

--- gatsby-starter/gatsby-config.js
+++ gatsby-starter/gatsby-config.js
@@ -74,48 +74,11 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken:process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken:process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
         environment: process.env.CONTENTFUL_ENV,
       }
     },
   ],
 }
 
-let contentfulConfig
 
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
-} catch (_) {}
-
-// Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN || contentfulConfig.accessToken,
-  host: process.env.CONTENTFUL_HOST
-}
-
-const { spaceId, accessToken } = contentfulConfig
-
-if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
-  )
-}
-
-module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Contentful starter',
-  },
-  pathPrefix: '/gatsby-contentful-starter',
-  plugins: [
-    'gatsby-transformer-remark',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
-    }
-  ],
-}
