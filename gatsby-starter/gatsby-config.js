5070dba27a29528cfb797a9d2d545cef708895ad
Dan <d_emery@outlook.com.au>
2020-01-04 04:38:26 +0800

    Update gatsby-config.js

--- gatsby-starter/gatsby-config.js
+++ gatsby-starter/gatsby-config.js
@@ -1,84 +1,41 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
+let contentfulConfig
+
+try {
+  // Load the Contentful config from the .contentful.json
+  contentfulConfig = require('./.contentful')
+} catch (_) {}
+
+// Overwrite the Contentful config with environment variables if they exist
+contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+  accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN || contentfulConfig.accessToken,
+  host: process.env.CONTENTFUL_HOST
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the delivery token need to be provided.'
+  )
+}
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter`,
-    description: `Gatsby Starter`,
-    author: `Polychronis Papadakis`,
+    title: 'Gatsby Contentful starter',
   },
+  pathPrefix: '/gatsby-contentful-starter',
   plugins: [
-    `gatsby-plugin-flow`,
-    `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-material-ui`,
-      options: {
-        // stylesProvider: {
-        //   injectFirst: true,
-        // },
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-module-resolver',
-      options: {
-        root: './src',
-        aliases: {
-          'components': './components',
-          'containers': './containers',
-          'images': './images',
-          'state': './state',
-          'styles': './styles',
-          'utils': './utils',
-          static: {
-            root: './public',
-            alias: './static'
-          }
-        }
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter`,
-        short_name: `Gatsby Starter`,
-        start_url: `/`,
-        background_color: `black`,
-        theme_color: `black`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-react-redux`,
-      options: {
-        pathToCreateStoreModule: "./src/state/store",
-        serialize: {
-          space: 0,
-          isJSON: true,
-          unsafe: false,
-        },
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken:process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-        environment: process.env.CONTENTFUL_ENV,
-      }
-    },
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
+    }
   ],
 }
-
-
